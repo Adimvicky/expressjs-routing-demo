1f3dd3e2529f8bbb59cea17355b968322a854943
Adim Victor <adimvicky@gmail.com>
2018-07-04 13:23:22 -0700

    Add app files

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended : true}))
+
+
+
+var entries = [
+  {
+    title : "Lorem Ipsum",
+    body : `Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed
+     do eiusmod tempor` ,
+    published : new Date().toDateString()
+   }
+];
+
+
+// The line below makes entries available in all views
+
+app.locals.entries = entries;
+
+
+app.get('/', function(req, res){
+  res.render('index.ejs')
+});
+
+
+app.get('/new', function(req, res){
+  res.render('new.ejs')
+});
+
+
+app.post('/new', checkPostValidity,function(req, res){
+   res.redirect('/')
+});
+
+app.use(function(req, res){
+  res.render("404.ejs")
+});
+
+
+
+function checkPostValidity(req, res, next){
+  var title = req.body.title;
+  var body = req.body.body;
+
+  if(!title || !body){
+    res.status(400);
+    res.end("Entry must have a title and body");
+    return;
+  } else {
+    entries.push(
+      {
+       title : title,
+       body : body,
+       published : new Date().toDateString()
+      }
+  );
+    next();
+  };
+}
+
+
+
+app.listen(3000, function(){
+  console.log("Server running...")
+})
